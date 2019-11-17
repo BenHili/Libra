3c6e24988903832b63e0480f183a37473f40f7a9
Ben Hili <ben.hili1994@gmail.com>
2019-11-17 13:59:01 +1100

    Adds more detail to result components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,20 @@ import React from "react";
 import "./App.css";
 
 import book from "./images/road_to_learn_react.jpg";
+
+const description = `Swipe at owner's legs purr like an angel. Crash against wall but walk
+        away like nothing happened so you're just gonna scroll by without saying
+        meowdy? yet love and coo around boyfriend who purrs and makes the
+        perfect moonlight eyes so i can purr and swat the glittery gleaming yarn
+        to him (the yarn is from a $125 sweater) for lick the plastic bag, cats
+        making all the muffins. When in doubt, wash always ensure to lay down in
+        such a manner that tail can lightly brush human's nose yet kitty
+        scratches couch bad kitty for 𝕄𝔼𝕆𝕎 sit in a box for hours.`;
+
 function Header(props) {
   return (
     <header className="App-header">
-      <h1 style={{ width: "auto" }}>{props.name}</h1>
+      <h1>{props.name}</h1>
       <input className="App-search" type="text" />
     </header>
   );
@@ -14,17 +24,8 @@ function Header(props) {
 function Result(props) {
   return (
     <div className="App-result">
-      <img src={book} />
-      <p>
-        Swipe at owner's legs purr like an angel. Crash against wall but walk
-        away like nothing happened so you're just gonna scroll by without saying
-        meowdy? yet love and coo around boyfriend who purrs and makes the
-        perfect moonlight eyes so i can purr and swat the glittery gleaming yarn
-        to him (the yarn is from a $125 sweater) for lick the plastic bag, cats
-        making all the muffins. When in doubt, wash always ensure to lay down in
-        such a manner that tail can lightly brush human's nose yet kitty
-        scratches couch bad kitty for 𝕄𝔼𝕆𝕎 sit in a box for hours.
-      </p>
+      <img src={props.image} />
+      <p>{props.description}</p>
     </div>
   );
 }
@@ -33,7 +34,7 @@ function App() {
   return (
     <div className="App">
       <Header name="📖 Libra" />
-      <Result />
+      <Result image={book} description={description} />
     </div>
   );
 }
