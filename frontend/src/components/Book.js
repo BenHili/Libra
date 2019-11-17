977d8b193215200b1a1e01cecafa2e171207bcd5
Karmar Barrett <karmar.barrett97@gmail.com>
2019-11-18 00:48:51 +1100

    create hover ransition for book cards

--- frontend/src/components/Book.js
+++ frontend/src/components/Book.js
@@ -4,9 +4,12 @@ import "./Book.css";
 function Book(props) {
   return (
     <div className="Book-card">
+    <span>
       <div className="Image-box">
         <img src={props.image} />
+        <a className="View-btn">View</a>
       </div>
+      </span>
       <div className="Text-box">
         <h2>{props.title}</h2>
         <p>{props.description}</p>
