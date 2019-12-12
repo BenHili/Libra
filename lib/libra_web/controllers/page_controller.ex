d8608142f8462f7f3935124d8df80896bf6e7ca4
BenHili <ben.hili1994@gmail.com>
2019-12-12 22:57:39 +1100

    feat: demo books info being sent from controller

--- lib/libra_web/controllers/page_controller.ex
+++ lib/libra_web/controllers/page_controller.ex
@@ -5,11 +5,42 @@ defmodule LibraWeb.PageController do
     render(conn, "index.html")
   end
 
-  def buy(conn, _params) do
-    render(conn, "results.html")
+  def buy(conn, %{"query" => query}) do
+    stubBooks = [
+      %{
+        :title => "Road to learn react",
+        :image => "/images/road_to_learn_react.jpg",
+        :description => "Blah blah",
+        :price => "$99.99"
+      },
+      %{
+        :title => "Programming Phoenix 14",
+        :image => "/images/programming_phoenix.jpg",
+        :description => "Blah blah",
+        :price => "Priceless"
+      },
+      %{
+        :title => "Elixir In Action",
+        :image => "/images/elixir_in_action.jpg",
+        :description => "My boi",
+        :price => "Priceless"
+      }
+
+    ]
+
+    render(conn, "results.html", books: stubBooks)
   end
 
-  def sell(conn, _params) do
-    render(conn, "results.html")
+  def sell(conn, %{"query" => query}) do
+    stubBooks = [
+      %{
+        :title => "Road to learn react",
+        :image => "/images/road_to_learn_react.jpg",
+        :description => "Blah blah",
+        :price => "$99.99"
+      }
+    ]
+
+    render(conn, "results.html", books: stubBooks)
   end
 end
